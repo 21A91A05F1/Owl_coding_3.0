be29bc434504a40744776a1e4b8a4f024d36b9d3
Naga Sathwika Kovvuri <97424376+21A91A05F1@users.noreply.github.com>
2024-01-12 23:24:40 +0530

    Create Graph_DFs.cpp

---
+++ Graph_DFs.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to return a list containing the DFS traversal of the graph.
+    void fun(int Node,vector<int> adj[], vector<int>&res,vector<int>&vis)
+    {
+        res.push_back(Node);
+        vis[0]=1;
+        for(auto it:adj[Node])
+        {
+            if(vis[it]==0)
+            {
+                vis[it]=1;
+                fun(it,adj,res,vis);
+                
+            }
+        }
+    }
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+        vector<int>vis(V,0);
+        vector<int>res;
+        fun(0,adj,res,vis);
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        // string s1;
+        // cin>>s1;
+        Solution obj;
+        vector<int> ans = obj.dfsOfGraph(V, adj);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
