750c2903c98c143336ade40d8a4d6777bd0523b0
Naga Sathwika Kovvuri <97424376+21A91A05F1@users.noreply.github.com>
2024-01-13 14:16:32 +0530

    Create LL_Count nodes of linked list.cpp

---
+++ LL_Count nodes of linked list.cpp
@@ -0,0 +1,13 @@
+
+int length(Node *head)
+{
+	//Write your code here
+    Node *temp=head;
+    int c=0;
+    while(temp!=NULL)
+    {
+        c++;
+        temp=temp->next;
+    }
+    return c;
+}
