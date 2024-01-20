73593ba64a0491fbebee8187c98066c02c8e178f
Naga Sathwika Kovvuri <97424376+21A91A05F1@users.noreply.github.com>
2024-01-21 01:41:17 +0530

    Create Tree_Binary Tree Right Side View.cpp

---
+++ Tree_Binary Tree Right Side View.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void fun(TreeNode* root,vector<int>&ve,int level )
+    {
+        if(root==NULL) return;
+        if(level==ve.size()) ve.push_back(root->val);
+        fun(root->right,ve,level+1);
+        fun(root->left,ve,level+1);
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int>res;
+        fun(root,res,0);
+        return res;
+    }
+};
